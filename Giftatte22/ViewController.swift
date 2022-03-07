177c6a7a4065036ced9865c3ee79d9979f9cac02
tngusmiso <k1miso012@gmail.com>
2022-03-07 23:41:42 +0900

    프로젝트 재생성

---
+++ Giftatte22/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Giftatte22
+//
+//  Created by 임수현 on 2022/03/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
