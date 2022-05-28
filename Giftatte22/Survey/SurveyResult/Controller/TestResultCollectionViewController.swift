ef11fcf0be448fc6d94e6de3f3713076fda71a75
DevKDuck <96559947+DevKDuck@users.noreply.github.com>
2022-05-28 23:14:11 +0900

    SurveyResult 결과창 Price선택시 가격대 맞게 데이터 띄우도록함

--- Giftatte22/Survey/SurveyResult/Controller/TestResultCollectionViewController.swift
+++ Giftatte22/Survey/SurveyResult/Controller/TestResultCollectionViewController.swift
@@ -76,61 +76,196 @@ extension TestResultCollectionViewController: UICollectionViewDelegateFlowLayout
         
         cell.testResultCollecionCell.backgroundColor = colors
         //        cell.testResultImageView.image = images[indexPath.row]
-        if let url = URL(string: resultGiftDataArray[indexPath.row].imageUrl){
-            if let imagedata = try? Data(contentsOf: url){
-                cell.testResultImageView.image = UIImage(data: imagedata)
-                
-                let bounds = cell.testResultImageView.bounds
-                let pathCircle = UIBezierPath(ovalIn: bounds)
-                
-                let layer = CAShapeLayer()
-                layer.path = pathCircle.cgPath
-                
-                cell.testResultImageView.layer.mask = layer
+        
+        
+        switch price{
+            
+        case "under10000" :
+            if resultGiftDataArray[indexPath.row].meanPrice < 10000 {
+                cell.testResultProductPriceLabel.text = String("\(resultGiftDataArray[indexPath.row].meanPrice)원")
+                if let url = URL(string: resultGiftDataArray[indexPath.row].imageUrl){
+                    if let imagedata = try? Data(contentsOf: url){
+                        cell.testResultImageView.image = UIImage(data: imagedata)
+                        
+                        let bounds = cell.testResultImageView.bounds
+                        let pathCircle = UIBezierPath(ovalIn: bounds)
+                        
+                        let layer = CAShapeLayer()
+                        layer.path = pathCircle.cgPath
+                        
+                        cell.testResultImageView.layer.mask = layer
+                    }
+                }
+                cell.testResultProductNameLabel.text = resultGiftDataArray[indexPath.row].keyword
             }
+            else{
+                cell.testResultProductNameLabel.text = "돈이 더 필요해"
+                cell.testResultProductPriceLabel.text = "ㅠㅠ"
+                cell.testResultImageView.image = UIImage(systemName: "xmark")
+            }
+        case "10000to20000" :
+            if resultGiftDataArray[indexPath.row].meanPrice < 20000 {
+                if resultGiftDataArray[indexPath.row].meanPrice > 10000{
+                    cell.testResultProductPriceLabel.text = String("\(resultGiftDataArray[indexPath.row].meanPrice)원")
+                    if let url = URL(string: resultGiftDataArray[indexPath.row].imageUrl){
+                        if let imagedata = try? Data(contentsOf: url){
+                            cell.testResultImageView.image = UIImage(data: imagedata)
+                            
+                            let bounds = cell.testResultImageView.bounds
+                            let pathCircle = UIBezierPath(ovalIn: bounds)
+                            
+                            let layer = CAShapeLayer()
+                            layer.path = pathCircle.cgPath
+                            
+                            cell.testResultImageView.layer.mask = layer
+                        }
+                    }
+                    cell.testResultProductNameLabel.text = resultGiftDataArray[indexPath.row].keyword
+                }else{
+                    cell.testResultProductNameLabel.text = "돈이 더 필요해"
+                    cell.testResultProductPriceLabel.text = "ㅠㅠ"
+                    cell.testResultImageView.image = UIImage(systemName: "xmark")
+                    
+                }
+            }else{
+                cell.testResultProductNameLabel.text = "돈이 더 필요해"
+                cell.testResultProductPriceLabel.text = "ㅠㅠ"
+                cell.testResultImageView.image = UIImage(systemName: "xmark")
+            }
+            
+        case "20000to30000" :
+            if resultGiftDataArray[indexPath.row].meanPrice < 30000 {
+                if resultGiftDataArray[indexPath.row].meanPrice > 20000{
+                    cell.testResultProductPriceLabel.text = String("\(resultGiftDataArray[indexPath.row].meanPrice)원")
+                    if let url = URL(string: resultGiftDataArray[indexPath.row].imageUrl){
+                        if let imagedata = try? Data(contentsOf: url){
+                            cell.testResultImageView.image = UIImage(data: imagedata)
+                            
+                            let bounds = cell.testResultImageView.bounds
+                            let pathCircle = UIBezierPath(ovalIn: bounds)
+                            
+                            let layer = CAShapeLayer()
+                            layer.path = pathCircle.cgPath
+                            
+                            cell.testResultImageView.layer.mask = layer
+                        }
+                    }
+                    cell.testResultProductNameLabel.text = resultGiftDataArray[indexPath.row].keyword
+                }else{
+                    cell.testResultProductNameLabel.text = "가격대가 안맞아요"
+                    cell.testResultImageView.image = UIImage(systemName: "xmark")
+                    cell.testResultProductPriceLabel.text = "ㅠㅠ"
+                }
+            }else{
+                cell.testResultProductNameLabel.text = "가격대가 안맞아요"
+                cell.testResultImageView.image = UIImage(systemName: "xmark")
+                cell.testResultProductPriceLabel.text = "ㅠㅠ"
+            }
+            
+            
+        case "30000to50000" :
+            if resultGiftDataArray[indexPath.row].meanPrice < 50000 {
+                if resultGiftDataArray[indexPath.row].meanPrice > 30000{
+                    cell.testResultProductPriceLabel.text = String("\(resultGiftDataArray[indexPath.row].meanPrice)원")
+                    if let url = URL(string: resultGiftDataArray[indexPath.row].imageUrl){
+                        if let imagedata = try? Data(contentsOf: url){
+                            cell.testResultImageView.image = UIImage(data: imagedata)
+                            
+                            let bounds = cell.testResultImageView.bounds
+                            let pathCircle = UIBezierPath(ovalIn: bounds)
+                            
+                            let layer = CAShapeLayer()
+                            layer.path = pathCircle.cgPath
+                            
+                            cell.testResultImageView.layer.mask = layer
+                        }
+                    }
+                    cell.testResultProductNameLabel.text = resultGiftDataArray[indexPath.row].keyword
+                }else{
+                    cell.testResultProductNameLabel.text = "가격대가 안맞아요"
+                    cell.testResultImageView.image = UIImage(systemName: "xmark")
+                    cell.testResultProductPriceLabel.text = "ㅠㅠ"
+                }
+            }else{
+                cell.testResultProductNameLabel.text = "가격대가 안맞아요"
+                cell.testResultImageView.image = UIImage(systemName: "xmark")
+                cell.testResultProductPriceLabel.text = "ㅠㅠ"
+            }
+            
+        default :
+            cell.testResultProductPriceLabel.text = String("\(resultGiftDataArray[indexPath.row].meanPrice)원")
+            if let url = URL(string: resultGiftDataArray[indexPath.row].imageUrl){
+                if let imagedata = try? Data(contentsOf: url){
+                    cell.testResultImageView.image = UIImage(data: imagedata)
+                    
+                    let bounds = cell.testResultImageView.bounds
+                    let pathCircle = UIBezierPath(ovalIn: bounds)
+                    
+                    let layer = CAShapeLayer()
+                    layer.path = pathCircle.cgPath
+                    
+                    cell.testResultImageView.layer.mask = layer
+                }
+            }
+            cell.testResultProductNameLabel.text = resultGiftDataArray[indexPath.row].keyword
         }
-        cell.testResultProductPriceLabel.text = String("\(resultGiftDataArray[indexPath.row].meanPrice)원")
-        cell.testResultProductNameLabel.text = resultGiftDataArray[indexPath.row].keyword
+        
+        //        if let url = URL(string: resultGiftDataArray[indexPath.row].imageUrl){
+        //            if let imagedata = try? Data(contentsOf: url){
+        //                cell.testResultImageView.image = UIImage(data: imagedata)
+        //
+        //                let bounds = cell.testResultImageView.bounds
+        //                let pathCircle = UIBezierPath(ovalIn: bounds)
+        //
+        //                let layer = CAShapeLayer()
+        //                layer.path = pathCircle.cgPath
+        //
+        //                cell.testResultImageView.layer.mask = layer
+        //            }
+        //        }
+        
+        //        cell.testResultProductPriceLabel.text = String("\(resultGiftDataArray[indexPath.row].meanPrice)원")
+        //        cell.testResultProductNameLabel.text = resultGiftDataArray[indexPath.row].keyword
         cell.layer.cornerRadius = 15
         
-
-    return cell
-}
-
-func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-//    let collectionViewWidth = collectionView.bounds.width
-//    let collectionViewHeight = collectionView.bounds.height
-    let cellSize = CGSize(width: widths[indexPath.row], height: heights[indexPath.row])
-    return cellSize
-}
-func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-    return 10
-}
-func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-    return 10
-}
-func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-    switch kind {
-    case UICollectionView.elementKindSectionHeader:
-        let headerView = testResultCollecionVeiw.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "TestResultCollectionReusableView", for: indexPath)
-        return headerView
-    default:
-        assert(false, "응 아니야")
+        
+        return cell
     }
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        //    let collectionViewWidth = collectionView.bounds.width
+        //    let collectionViewHeight = collectionView.bounds.height
+        let cellSize = CGSize(width: widths[indexPath.row], height: heights[indexPath.row])
+        return cellSize
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        switch kind {
+        case UICollectionView.elementKindSectionHeader:
+            let headerView = testResultCollecionVeiw.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "TestResultCollectionReusableView", for: indexPath)
+            return headerView
+        default:
+            assert(false, "응 아니야")
+        }
+        
+        
+    }
     
-}
-    
-func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         if let encoded =
             resultGiftDataArray[indexPath.row].webUrl.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed), let resultURL = URL(string: encoded){
             UIApplication.shared.open(resultURL, options: [:])
         }
     }
-func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
-    let width: CGFloat = testResultCollecionVeiw.frame.width
-    let height: CGFloat =  150
-    return CGSize(width: width, height: height)
-}
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        let width: CGFloat = testResultCollecionVeiw.frame.width
+        let height: CGFloat =  150
+        return CGSize(width: width, height: height)
+    }
 }
 
