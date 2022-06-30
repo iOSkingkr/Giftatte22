64a36058446ba294c212001ac26b0dee7f9d6aba
DevKDuck <loading95@naver.com>
2022-06-30 18:52:06 +0900

    Main 카드뉴스 비율-> 높이,넓이 지정

--- Giftatte22/Main/Main Recommend/Cell/MainRecommendCollectionViewCell.swift
+++ Giftatte22/Main/Main Recommend/Cell/MainRecommendCollectionViewCell.swift
@@ -50,12 +50,12 @@ class MainRecommendCollectionViewCell: UICollectionViewCell {
         
         self.customTopLabel.translatesAutoresizingMaskIntoConstraints = false
         self.customTopLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20).isActive = true
-        self.customTopLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 340).isActive = true
+        self.customTopLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 310).isActive = true
        
         self.customBottomLabel.translatesAutoresizingMaskIntoConstraints = false
         self.customBottomLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor,constant: 20).isActive = true
         self.customBottomLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor,constant: 20).isActive = true
-        self.customBottomLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 380).isActive = true
+        self.customBottomLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 350).isActive = true
         
     
         
