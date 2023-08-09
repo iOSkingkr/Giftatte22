4f01964edc1ccf3b155a4d954ee0ca4a243a1977
DevKDuck <loading95@naver.com>
2023-08-09 16:13:43 +0900

    MainRecommend에 보여줄 Image들의 Static 속성 제거

--- Giftatte22/Main/Main Recommend/Controller/Images.swift
+++ Giftatte22/Main/Main Recommend/Controller/Images.swift
@@ -10,41 +10,41 @@ import UIKit
 struct Images {
     
     // mainRecommendCollectionView image
-    static let parentsImage: UIImage = UIImage(named:"parentsImage.png")!
-    static let summerImage: UIImage = UIImage(named:"summerImage.png")!
-    static let twentyMenImage: UIImage = UIImage(named:"twentyMenImage.png")!
-    static let twentyWomenImage: UIImage = UIImage(named:"twentyWomanImage.png")!
-    static let uselessImage: UIImage = UIImage(named:"uselessImage.png")!
+    let parentsImage: UIImage = UIImage(named:"parentsImage.png")!
+    let summerImage: UIImage = UIImage(named:"summerImage.png")!
+    let twentyMenImage: UIImage = UIImage(named:"twentyMenImage.png")!
+    let twentyWomenImage: UIImage = UIImage(named:"twentyWomanImage.png")!
+    let uselessImage: UIImage = UIImage(named:"uselessImage.png")!
     
     // mainRecommendResult image
-    static let noBGParentsGiftImage: UIImage = UIImage(named:"noBGParentsGiftImage.png")!
-    static let noBGSummerGiftImage: UIImage = UIImage(named:"noBGSummerGiftImage.png")!
-    static let noBGTwentyMenGiftImage: UIImage = UIImage(named:"noBGTwentyMenGiftImage.png")!
-    static let noBGTwentyWomenGiftImage: UIImage = UIImage(named:"noBGTwentyWomenGiftImage.png")!
-    static let noBGUselessGiftImage: UIImage = UIImage(named:"noBGUselessGiftImage.png")!
+    let noBGParentsGiftImage: UIImage = UIImage(named:"noBGParentsGiftImage.png")!
+    let noBGSummerGiftImage: UIImage = UIImage(named:"noBGSummerGiftImage.png")!
+    let noBGTwentyMenGiftImage: UIImage = UIImage(named:"noBGTwentyMenGiftImage.png")!
+    let noBGTwentyWomenGiftImage: UIImage = UIImage(named:"noBGTwentyWomenGiftImage.png")!
+    let noBGUselessGiftImage: UIImage = UIImage(named:"noBGUselessGiftImage.png")!
     
     // hot_category
-    static let y_baby: UIImage = UIImage(named: "y_baby.png")!
-    static let y_book: UIImage = UIImage(named: "y_book.png")!
-    static let y_clothes: UIImage = UIImage(named: "y_clothes.png")!
-    static let y_computer: UIImage = UIImage(named: "y_computer.png")!
-    static let y_cosmetics: UIImage = UIImage(named: "y_cosmetics.png")!
-    static let y_giftcard: UIImage = UIImage(named: "y_giftcard.png")!
-    static let y_interior: UIImage = UIImage(named: "y_interior.png")!
-    static let y_lifeitem: UIImage = UIImage(named: "y_lifeitem.png")!
-    static let y_watch: UIImage = UIImage(named: "y_watch.png")!
+    let y_baby: UIImage = UIImage(named: "y_baby.png")!
+    let y_book: UIImage = UIImage(named: "y_book.png")!
+    let y_clothes: UIImage = UIImage(named: "y_clothes.png")!
+    let y_computer: UIImage = UIImage(named: "y_computer.png")!
+    let y_cosmetics: UIImage = UIImage(named: "y_cosmetics.png")!
+    let y_giftcard: UIImage = UIImage(named: "y_giftcard.png")!
+    let y_interior: UIImage = UIImage(named: "y_interior.png")!
+    let y_lifeitem: UIImage = UIImage(named: "y_lifeitem.png")!
+    let y_watch: UIImage = UIImage(named: "y_watch.png")!
     
     
     
     // survey 
-    static let baby: UIImage = UIImage(named: "baby.png")!
-    static let book: UIImage = UIImage(named: "book.png")!
-    static let clothes: UIImage = UIImage(named: "clothes.png")!
-    static let computer: UIImage = UIImage(named: "computer.png")!
-    static let cosmetics: UIImage = UIImage(named: "cosmetics.png")!
-    static let giftcard: UIImage = UIImage(named: "giftcard.png")!
-    static let interior: UIImage = UIImage(named: "interior.png")!
-    static let lifeitem: UIImage = UIImage(named: "lifeitem.png")!
-    static let watch: UIImage = UIImage(named: "watch.png")!
+    let baby: UIImage = UIImage(named: "baby.png")!
+    let book: UIImage = UIImage(named: "book.png")!
+    let clothes: UIImage = UIImage(named: "clothes.png")!
+    let computer: UIImage = UIImage(named: "computer.png")!
+    let cosmetics: UIImage = UIImage(named: "cosmetics.png")!
+    let giftcard: UIImage = UIImage(named: "giftcard.png")!
+    let interior: UIImage = UIImage(named: "interior.png")!
+    let lifeitem: UIImage = UIImage(named: "lifeitem.png")!
+    let watch: UIImage = UIImage(named: "watch.png")!
     
 }
